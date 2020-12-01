e95109a9ed925c4a57e0ce34d963bc92965147db
Aditya Pimparkar <adityapimparkar22@gmail.com>
2020-12-01 21:05:27 +0530

    Orc Reader Class created

--- OrcReaderWriter/src/main/java/OrcReader.java
+++ OrcReaderWriter/src/main/java/OrcReader.java
@@ -4,15 +4,9 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hive.ql.io.orc.RecordReader;
 import org.apache.hadoop.hive.serde2.objectinspector.StructField;
 import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
-
-import org.apache.log4j.LogSF;
-import org.apache.log4j.Logger;
 import org.apache.hadoop.hive.ql.io.orc.OrcFile;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 import java.util.concurrent.Callable;
 
 import org.apache.hadoop.hive.ql.io.orc.Reader;
@@ -22,9 +16,11 @@ public class OrcReader implements Runnable {
     private static final Configuration CONF = new Configuration();
     private Path path;
     private Queue queue;
-    public OrcReader(Path path, Queue queue){
+    private Queue jsonStrings;
+    public OrcReader(Path path, Queue queue,Queue jsonStrings){
         this.path=path;
         this.queue=queue;
+        this.jsonStrings=jsonStrings;
     }
     @Override
     public void run() {
@@ -32,26 +28,32 @@ public class OrcReader implements Runnable {
         try {
             Reader reader= OrcFile.createReader(path.getFileSystem(CONF),path);
             StructObjectInspector inspector = (StructObjectInspector)reader.getObjectInspector();
-            System.out.println(reader.getMetadata());
+        //    System.out.println(reader.getMetadata());
             RecordReader records = reader.rows();
             Object row = null;
             List fields = inspector.getAllStructFieldRefs();
-         /*   for(int i = 0; i < fields.size(); ++i) {
-                System.out.print(((StructField)fields.get(i)).getFieldObjectInspector().g + '\t');
-            }*/
+         //   System.out.println("header "+((StructObjectInspector) reader.getObjectInspector()));
+            List header = new ArrayList();
+           for(int i = 0; i < fields.size(); ++i) {
+            //    System.out.print(((StructField)fields.get(i)).getFieldName() + '\t');
+               header.add(((StructField)fields.get(i)).getFieldName());
+            }
             while (records.hasNext()) {
                 row = records.next(row);
                 List value_lst = inspector.getStructFieldsDataAsList(row);
-                StringBuilder builder=new StringBuilder();
-                for(Object value : value_lst){
-                    if(value!= null){
-                        builder.append(value.toString());
+             //   StringBuilder builder=new StringBuilder();
+                List<Map<String, Object>> lis = new LinkedList<>();
+                Map<String, Object> map = new HashMap<>();
+                for(int j=0;j<value_lst.size();j++){
+
+                    if(value_lst.get(j) != null){
+                        map.put((String) header.get(j),value_lst.get(j));
+                      //  builder.append(value.toString());
+                        lis.add(map);
                     }
-                    builder.append('\t');
+                    //builder.append('\t');
                 }
-                StringBuilder builder1=new StringBuilder("Builder ");
-                System.out.println(builder1.append(builder));
-                queue.add(value_lst);
+                queue.add(map);
             }
         } catch (IOException e) {
             e.printStackTrace();
