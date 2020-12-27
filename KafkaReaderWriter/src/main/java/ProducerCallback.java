c4ebe3ae0094c015841a9157e59dd87cc3a5c4f5
Aditya Pimparkar <adityapimparkar22@gmail.com>
2020-12-27 17:40:09 +0530

    Producer Callback class created

---
+++ KafkaReaderWriter/src/main/java/ProducerCallback.java
@@ -0,0 +1,12 @@
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+public class ProducerCallback implements Callback {
+
+    @Override
+    public void onCompletion(RecordMetadata metadata, Exception exception) {
+        if(exception!=null){
+            exception.printStackTrace();
+        }
+    }
+}
